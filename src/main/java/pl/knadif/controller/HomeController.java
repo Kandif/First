366bf4cf1c7d099d5c2fd163bd31a8d1ab1b2ce3
Krzysztof Kłak <kk@sellintegro.com>
2019-11-29 21:37:29 +0100

    Pierwszy commit

--- src/main/java/pl/knadif/controller/HomeController.java
+++ src/main/java/pl/knadif/controller/HomeController.java
@@ -3,10 +3,11 @@ package pl.knadif.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(){
         return "Hello World";
     }
